7fa24035740db580b309ad6a8f9bd4a5e32745e8
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2022-07-24 18:45:56 +0300

    change_class_to_hook

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const ContainerBody = styled.div`
+  /* background: url(http://img6.uploadhouse.com/fileuploads/17737/17737075bdad5cfa9423bb2c3c2beee695c72bb8.jpg); */
+  background: linear-gradient(to bottom, #0f0c29, #302b63, #24243e);
+  /* height: 100vh; */
+  width: 100vw;
+  text-align: center;
+`;
+
+export const Title = styled.h1`
+  margin: 0;
+  color: #fff;
+`;
+
+export const TitleSecond = styled.h2`
+  margin: 0;
+  color: #fff;
+`;
